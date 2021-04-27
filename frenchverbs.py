b745f1581f541eed3383867af2feab561d41a554
Jason <jasonyip1234567@gmail.com>
2021-04-27 13:24:08 -0700

    [FIX]: Changed to proper JSON

--- frenchverbs.py
+++ frenchverbs.py
@@ -4,8 +4,8 @@ import re
 import queue
 import sys
 import pprint
-from openpyxl import Workbook
 import datetime
+import json
 
 
 class WordReference:
@@ -41,7 +41,7 @@ class WordReference:
                                 from_word_tag.strong.text).strip()
                         if main_translation not in translations:
                             translations[main_translation] = []
-                        translations[main_translation].append(set())
+                        translations[main_translation].append([])
 
                 if main_translation is not None and "(UK)":
                     possible_dialect = translation.find(attrs = { "class" : "To2" })
@@ -49,7 +49,7 @@ class WordReference:
                         continue
                     for unnecessary_tag in to_word_tag.find_all(["a", "em"]):
                         unnecessary_tag.decompose()
-                    translations[main_translation][-1].update(
+                    translations[main_translation][-1].extend(
                             [re.sub(r"\s(?=\s|$)|/s\w{1,2}", "", synonym).strip() for synonym in
                             to_word_tag.text.split(",")])
 
@@ -79,6 +79,7 @@ if __name__ == "__main__":
     wr = WordReference()
     with open(sys.argv[1], 'r', encoding='utf8') as f:
         manual_translations = dict([ translation.split("\t") for translation in f.readlines() ])
+    """
     with open(sys.argv[2], 'r', encoding='utf8') as f:
         verbs = f.readlines()
     automatic_translations = {}
@@ -88,21 +89,16 @@ if __name__ == "__main__":
         verbs[i] = verb
         print(verb)
         automatic_translations.update(wr.scrape(verb))
-
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(automatic_translations)
-    wb = Workbook()
-    wb.create_sheet("Automated")
-    wb.create_sheet("Manual")
-    for k, v in automatic_translations.items():
-        wb["Automated"].append([k] + [','.join(synonyms) for synonyms in v])
+    """
+    #pp = pprint.PrettyPrinter(indent=4)
+    #pp.pprint(automatic_translations)
+    """
+    with open(datetime.datetime.now().strftime("%y%m%d_%H%M%S") + "_automatic.json", "w",
+            encoding="utf-8") as f:
+        json.dump(automatic_translations, f, ensure_ascii=False)
+    """
     for k, v in manual_translations.items():
-        wb["Manual"].append([k, ','.join(parse_manual(v))])
-
-    if 'Sheet' in wb.sheetnames:
-        wb.remove(wb['Sheet'])
-    wb.save(datetime.datetime.now().strftime("%y%m%d_%H%M%S") + ".xlsx")
-    #print(automatic_translations["changer"])
-
-    #print(manual_translations["changer"])
-
+        manual_translations[k] = parse_manual(v)
+    with open(datetime.datetime.now().strftime("%y%m%d_%H%M%S") + "_manual.json", "w",
+            encoding="utf-8") as f:
+        json.dump(manual_translations, f, ensure_ascii=False)
