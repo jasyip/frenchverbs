2f26de05188a984549af0309e5ab6da8a0324ae2
fishy15 <aaryan.prakash3.14@gmail.com>
2021-04-29 10:32:06 -0700

    [FIX]: Increased speed of filtering

--- filter_list.py
+++ filter_list.py
@@ -3,14 +3,16 @@ import sys
 import datetime
 from queue import PriorityQueue
 import re
+import string
 
 class State:
-    def __init__(self, words, length):
+    def __init__(self, words, length, word_length):
         self.words = words
         self.length = length
+        self.word_length = word_length
 
     def copy(self):
-        return State(self.words.copy(), self.length)
+        return State(self.words.copy(), self.length, self.word_length)
 
     def insert(self, word):
         for cur_word in self.words:
@@ -18,39 +20,37 @@ class State:
                 break
         else:
             self.words.append(word)
+            self.word_length += len(list(c for c in word if c not in string.whitespace))
         self.length += 1
 
     def __repr__(self):
-        return repr((self.words, self.length))
+        return repr((self.words, self.length, self.word_length))
 
     def __len__(self):
-        return len(re.findall(r"\w", ''.join(self.words), re.U))
+        return self.word_length
 
     def __lt__(self, other):
-        if len(self) < len(other):
-            return True
-        self.words < other.words
-
+        if len(self) == len(other):
+            return self.words < other.words
+        return len(self) < len(other) 
 
 def filter_def(def_list):
     pq = PriorityQueue()
-    pq.put(State([], 0))
-    ans = []
+    pq.put(State([], 0, 0))
 
-    while not pq.empty():
-        cur = pq.get()
+    while pq:
+        cur = pq.get() 
         idx = cur.length
 
         if idx == len(def_list):
-            if not ans or len(cur.words) < len(ans):
-                ans = cur.words
+            return cur.words
         else:
             for word in def_list[idx]:
                 nxt_state = cur.copy()
                 nxt_state.insert(word)
                 pq.put(nxt_state)
 
-    return ans
+    return []
 
 def main():
     with open(sys.argv[1], 'r', encoding="utf-8") as in_file:
@@ -59,8 +59,8 @@ def main():
         output_data = {}
         for word in data:
             #for word in dict(list(data.items())[: 2]):
-            output_data[word] = filter_def(data[word])
             print(word)
+            output_data[word] = filter_def(data[word])
 
     with open(datetime.datetime.now().strftime("%y%m%d_%H%M%S") + "_filtered.json",
             'w', encoding = "utf-8") as out_file:
